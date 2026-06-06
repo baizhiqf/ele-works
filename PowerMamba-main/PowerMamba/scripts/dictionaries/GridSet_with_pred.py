7541328a950143348dfb3cee0ff83e3f0a1f20a0
hchling241003 <hchling241003@ds.com>
2026-06-06 15:01:55 +0800

    Remove PowerMamba source code from tracking

--- PowerMamba-main/PowerMamba/scripts/dictionaries/GridSet_with_pred.py
+++
@@ -1,5 +0,0 @@
-project_dict = {
-'COAST': [241, 97], 'NCENT': [245, 25], 'FWEST': [243, 73], 'EAST': [242, 49], 'SOUTH': [246, 169], 'SOLAR': [254, 193], 'NORTH': [244, 1], 'WEST': [248, 145], 'WIND': [253, 217], 'SCENT': [247, 121]
-}
-
-col_info_dict = {'anc_serv': [-14, 4], 'price': [-8, 8], 'wind': [-10, 1], 'solar': [-9, 1], 'load': [-22, 8]}
